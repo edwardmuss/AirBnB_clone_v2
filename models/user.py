d3315356e55ea53e501a6c0be3d2479758472f08
topisteronyango <t.onyango@alustudent.com>
2021-12-20 04:31:27 -0800

    update user

--- models/user.py
+++ models/user.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-from models import *
+"""This is the user class."""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    __tablename__ = "users"
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-
-    places = relationship("Place", backref="user",
-                          cascade="all, delete, delete-orphan")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    reviews = relationship('Review', backref='user',
+                           cascade='delete')
+    places = relationship('Place', backref='user',
+                          cascade='delete')
