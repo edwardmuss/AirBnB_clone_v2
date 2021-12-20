c93f80a77a166a7f757981f461162f9d05905194
topisteronyango <t.onyango@alustudent.com>
2021-12-20 07:55:22 -0800

    test sql update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,17 @@
--- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
-
+-- Write a script that prepares a MySQL server --
+-- Create db 'hbnb_test_db' --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Add new user 'hbnb_test' in localhost --
+-- User 'hbnb_test' password should be 'hbnb_test_pwd' --
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+
+-- User 'hbnb_test' should have all privileges on db 'hbnb_test_db' --
+GRANT ALL PRIVILEGES ON hbnb_test_db.*
+TO 'hbnb_test'@'localhost';
+
+-- User 'hbnb_test' should have SELECT privileges on db 'performance_schema --
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
