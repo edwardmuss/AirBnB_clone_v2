1071c1874624d7e1dc20a454e65eaf182da47f0c
topisteronyango <t.onyango@alustudent.com>
2021-12-20 07:41:18 -0800

    amenity update

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from models.base_model import BaseModel, Base
+from models import *
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place',
-                                   secondary='place_amenity',
-                                   backref='amenities',
-                                   cascade='delete')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
