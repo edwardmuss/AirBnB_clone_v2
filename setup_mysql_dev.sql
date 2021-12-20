49ad09d516fb3df520a9fb1c42769164d4f7160b
topisteronyango <t.onyango@alustudent.com>
2021-12-20 08:07:21 -0800

    dev sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,19 +1,17 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
---   Grants all privileges for hbnb_dev on hbnb_dev_db.
---   Grants SELECT privilege for hbnb_dev on performance_schema.
-
+-- Write a script that prepares a MySQL server --
+-- Create db 'hbnb_dev_db' --
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Add new user 'hbnb_dev' in localhost --
+-- User 'hbnb_dev' password should be 'hbnb_dev_pwd' --
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- User 'hbnb_dev' should have SELECT privileges on db 'performance_schema' --
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+
+-- User 'hbnb_dev' should have all privileges on db 'hbnb_dev_db' --
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
