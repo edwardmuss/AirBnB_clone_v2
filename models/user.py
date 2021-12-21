3e2578c1fd3ba47b1a436900d62ff978de46329e
edwardmuss <edwardmuss5@gmail.com>
2021-12-21 10:36:15 +0300

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,36 @@
 #!/usr/bin/python3
-"""This is the user class."""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
+    """ Represent a user for a MySQL database.
+
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+        __tablename__: represents the table name, users
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The user-Place relationship.
+        reviews (sqlalchemy relationship): The user-Review relationship.
+
     """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    reviews = relationship('Review', backref='user',
-                           cascade='delete')
-    places = relationship('Place', backref='user',
-                          cascade='delete')
+
+    __tablename__ = "users"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
